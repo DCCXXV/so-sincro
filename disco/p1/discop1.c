8dc25a446df58cbd9d263496b9995402836ab3de
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2025-04-24 23:16:41 +0200

    Práctica 5 + contenidos Pr1

--- disco/p1/discop1.c
+++ disco/p1/discop1.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <pthread.h>
 
 #define CAPACITY 5
 #define VIPSTR(vip) ((vip) ? "  vip  " : "not vip")
 
-/** 
- *  Esta implementación añade un atributo para el turno en vez de usar el id del cliente
- */
+FILE* salida;
+
 typedef struct {
     int id;
     int is_vip;
-    int turno;
 }t_cliente;
 
 // recordar inicializar y destruir en el main SIEMPRE
@@ -30,25 +29,24 @@ int turno_actual_vips = 0;
 
 int numero_clientes_bailando = 0;
 
-void enter_vip_client(int id, int turno) {
+void enter_vip_client(int id) {
     pthread_mutex_lock(&mu); 
     numero_clientes_vips_cola++;
     // Esperar si:
     // 1. No es mi turno
     // 2. Esta lleno
-    while (turno != turno_actual_vips 
+    while (id != turno_actual_vips 
              || numero_clientes_bailando >= CAPACITY) {
-	    printf("#  Cliente esperando:   vip   con id:%2d\n", id);
+	    fprintf(salida, "#  Cliente esperando:   vip   con id:%2d\n", id);
         pthread_cond_wait(&sala_espera_vips, &mu);
-    }
-    numero_clientes_bailando++;
+    } numero_clientes_bailando++;
     numero_clientes_vips_cola--;
     turno_actual_vips++;
-    printf("-> Cliente entra    :   vip   con id: %d\n", id);
+    fprintf(salida, "-> Cliente entra    :   vip   con id: %d\n", id);
     pthread_mutex_unlock(&mu);
 }
 
-void enter_normal_client(int id, int turno) {
+void enter_normal_client(int id) {
     pthread_mutex_lock(&mu); 
     numero_clientes_normales_cola++;
     // Esperar si:
@@ -56,27 +54,27 @@ void enter_normal_client(int id, int turno) {
     // 2. No es mi turno
     // 3. Esta lleno
     while (numero_clientes_vips_cola != 0 
-            || turno != turno_actual_normales 
+            || id != turno_actual_normales 
             || numero_clientes_bailando >= CAPACITY) {
-	    printf("#  Cliente esperando: not vip con id:%2d\n", id);
+	    fprintf(salida, "#  Cliente esperando: not vip con id:%2d\n", id);
         pthread_cond_wait(&sala_espera_normales, &mu);
     }
     numero_clientes_bailando++;
     numero_clientes_normales_cola--;
     turno_actual_normales++;
-	printf("-> Cliente entra    : not vip con id:%2d\n", id);
+	fprintf(salida, "-> Cliente entra    : not vip con id:%2d\n", id);
     pthread_mutex_unlock(&mu);
 }
 
 void dance(int id, int isvip) {
-	printf("&  Cliente bailando : %s con id:%2d\n", VIPSTR(isvip), id);
+	fprintf(salida, "&  Cliente bailando : %s con id:%2d\n", VIPSTR(isvip), id);
 	sleep((rand() % 5) + 1);
 }
 
 void disco_exit(int id, int isvip) {
     pthread_mutex_lock(&mu); 
     numero_clientes_bailando--;
-    printf("<- Cliente fuera    : %s con id: %d\n", VIPSTR(isvip), id);
+    fprintf(salida, "<- Cliente fuera    : %s con id: %d\n", VIPSTR(isvip), id);
     if (numero_clientes_vips_cola > 0) {
         pthread_cond_signal(&sala_espera_vips); 
     }
@@ -89,48 +87,80 @@ void disco_exit(int id, int isvip) {
 void *client(void *arg) {
     t_cliente *cliente = (t_cliente*) arg;
     if (cliente->is_vip) {
-        enter_vip_client(cliente->id, cliente->turno);
+        enter_vip_client(cliente->id);
     }
     else {
-        enter_normal_client(cliente->id, cliente->turno);
+        enter_normal_client(cliente->id);
     }
     dance(cliente->id, cliente->is_vip);
     disco_exit(cliente->id, cliente->is_vip);
     pthread_exit(NULL);
 }
 
+void usage(char* progname) {
+    printf("Uso: %s -f <file_name> \n", progname);
+    printf("Opciones;\n");
+    printf("\t-f <file_name>: especificar el archivo de entrada\n");
+    printf("\t-o <file_name>: especificar el archivo de salida\n");
+    printf("\t-h: mostrar este mensaje\n");
+    printf("\t<nombre_discoteca>: mostrará el nombre de la discoteca al inicio");
+}
+
 int main(int argc, char *argv[]) {
     pthread_mutex_init(&mu, NULL);
     pthread_cond_init(&sala_espera_normales, NULL);
     pthread_cond_init(&sala_espera_vips, NULL);
-
-    FILE *fichero = fopen(argv[1], "r");
-    if (fichero == NULL) {
-        perror("Error abriendo el archivo.");
-        exit(EXIT_FAILURE);
-    }
     
+    int opt;
+    FILE* entrada;
+    
+    while ((opt = getopt(argc, argv, "f:o:h")) != -1) {
+        switch(opt) {
+            case 'f':
+                entrada=fopen(optarg, "r");
+                if (entrada == NULL) {
+                    perror("Error abriendo el archivo de entrada.");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case 'o':
+                salida=fopen(optarg, "w+");
+                if (salida == NULL) {
+                    perror("Error abriendo el archivo de salida.");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case 'h':
+                usage(argv[0]);
+                exit(0);
+        }
+    }
+    if (salida == NULL) salida = stdout;
+
+    if (optind < argc) {
+        fprintf(salida, "Discoteca %s\n", argv[optind]);
+    }
+     
     int total_cola;
-    fscanf(fichero, "%d", &total_cola);
+    fscanf(entrada, "%d", &total_cola);
     
     t_cliente clientes[total_cola];
     pthread_t hilos[total_cola];
     
     for (int i = 0; i < total_cola; i++) {
-        fscanf(fichero, "%d", &clientes[i].is_vip); 
+        fscanf(entrada, "%d", &clientes[i].is_vip); 
         if (clientes[i].is_vip) {
-            clientes[i].turno = turno_actual_vips++;
+            clientes[i].id = turno_actual_vips++;
         } else {
-            clientes[i].turno = turno_actual_normales++;
+            clientes[i].id = turno_actual_normales++;
         }
-        clientes[i].id = i;
-        printf("Cliente leído: %s con id: %d\n", VIPSTR(clientes[i].is_vip), clientes[i].id);
+        fprintf(salida, "Cliente leído: %s con id: %d\n", VIPSTR(clientes[i].is_vip), clientes[i].id);
     }
 
     turno_actual_normales = 0;
     turno_actual_vips = 0;
     
-    printf("TOTAL-------------------------%2d\n", total_cola);
+    fprintf(salida, "TOTAL-------------------------%2d\n", total_cola);
     
     // siempre un bucle para crearlos a todos
     for (int i = 0; i < total_cola; i++) {
@@ -140,6 +170,9 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < total_cola; i++) {
         pthread_join(hilos[i], NULL);
     }
+
+    fclose(entrada);
+    if (salida != stdout) fclose(salida);
 
     pthread_cond_destroy(&sala_espera_normales);
     pthread_cond_destroy(&sala_espera_vips);
